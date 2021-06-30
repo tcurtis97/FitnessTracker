3f3dad89fee3ff1ec7f85dcf6c204d84416894df
Tim Curtis <timcurtis97@yahoo.com>
2021-06-30 17:05:38 -0500

    added userprofile model

---
+++ FitnessTracker/Models/UserProfile.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FitnessTracker.Models
+{
+    public class UserProfile
+    {
+        public int Id { get; set; }
+
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+
+        [DisplayName("Display Name")]
+        public string DisplayName { get; set; }
+        public string Email { get; set; }
+
+        [DisplayName("Creation Date")]
+        public DateTime CreateDateTime { get; set; }
+
+        [DisplayName("Image")]
+        public string ImageLocation { get; set; }
+        public int UserTypeId { get; set; }
+        public UserType UserType { get; set; }
+
+        [DisplayName("Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
+
+
+    }
+}
